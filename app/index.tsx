6d56b7461f41e77fc569d0498914d1933fa38832
IT21312380 <it21312380@my.sliit.lk>
2024-11-30 13:18:08 +0530

    camera screen added

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React,{ View, Text } from 'react-native'
+import CameraScreen from '../app/screens/CameraScreen';
+
+
+
+const index = () => {
+  return (
+    <CameraScreen/>
+    
+  )
+}
+
+export default index
